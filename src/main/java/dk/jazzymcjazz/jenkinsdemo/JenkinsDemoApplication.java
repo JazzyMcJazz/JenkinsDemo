13e64e0a602d92c520f71bb3d024a90b02ece664
Lars <larsriel@gmail.com>
2021-09-03 16:59:09 +0200

    removed comment

--- src/main/java/dk/jazzymcjazz/jenkinsdemo/JenkinsDemoApplication.java
+++ src/main/java/dk/jazzymcjazz/jenkinsdemo/JenkinsDemoApplication.java
@@ -5,8 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JenkinsDemoApplication {
-    
-    // this is the main class
 
     public static void main(String[] args) {
         SpringApplication.run(JenkinsDemoApplication.class, args);
