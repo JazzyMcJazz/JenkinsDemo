49649a38b3f165fad18bea9b1071ec2c2088d059
Lars <larsriel@gmail.com>
2021-09-03 17:10:45 +0200

    test

--- src/main/java/dk/jazzymcjazz/jenkinsdemo/JenkinsDemoApplication.java
+++ src/main/java/dk/jazzymcjazz/jenkinsdemo/JenkinsDemoApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JenkinsDemoApplication {
 
-    // Comment
+
 
     public static void main(String[] args) {
         SpringApplication.run(JenkinsDemoApplication.class, args);
